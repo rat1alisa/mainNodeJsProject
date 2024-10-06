560f008c5a16d751e7b9f26eddc550c9c13a1725
AlᎥce <100071513+rat1alisa@users.noreply.github.com>
2024-10-06 20:03:30 +0300

    first

---
+++ log in/login.js
@@ -0,0 +1,122 @@
+
+const cleanInput = () => {
+    const getInput = doсument.getElementById("text");
+    getInput.value = '';                      
+  }
+
+
+  /* axios({
+  method: 'post',
+  url: '/login',
+  data: {
+    firstname: 'Alice',
+    lastname: 'Alekseeva',
+  }
+});*/
+
+
+/*AXIOS POST LOGIN*/
+
+axios.post('/login', {
+  email: '',
+  password: '',
+})
+.then((response) => {
+  console.log(response);
+}) 
+.catch(error => {
+  console.log(error);
+});
+
+
+/*AXIOS GET LOGIN*/
+
+const params = {
+  email: '',
+  password: '',
+};
+
+axios.get('https://api.example.com/', {params});
+
+
+/*FETCH LOGIN POST*/
+
+let login = {
+  email: '',
+  password: '',
+};
+
+let response = await fetch('/article/fetch/post/user', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'aplication/json;charset=utf-8',
+  },
+  body: JSON.stringify(login)
+});
+
+let result = await response.json();
+alert(result.massage);
+
+/*FETCH LOGIN GET*/
+
+fetch('https:://example.com/ajax.php')
+.then(response => {
+  return response.json();
+})
+.then (data => {
+  console.dir(data);
+});
+
+/*JS LOGIN POST*/
+
+const https = require('https');
+const data = JSON.stringify ({
+  email: '',
+  password: '',
+});
+const options = {
+  hostname: 'flaviocopes.com',
+  port: 443,
+  path: '/todos',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'aplication/json',
+    'Content-Lenght': data.length,
+  }
+}
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  res.on ('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+req.on('error', (error) => {
+  console.error(error)
+})
+req.write(data)
+req.end()
+
+
+/*JS LOGIN GET*/
+
+const https = require('https');
+const data1 = JSON.stringify ({
+  email: '',
+  password: '',
+});
+const options1 = {
+  hostname: 'flaviocopes.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET'
+}
+const req1 = https.request(options1, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  res.on ('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+req1.on('error', (error) => {
+  console.error(error)
+})
+req1.end()
---
+++ registration/registration.js
@@ -0,0 +1,116 @@
+
+const cleanInput = () => {
+    const getInput = doсument.getElementById("text");
+    getInput.value = '';                      
+}
+
+//registration Js
+
+//AXIOS POST
+
+axios.post('/registration', {
+  name: '',
+  surname: '',
+  dateOfBirth: '',
+  password: '',
+})
+.then((registration_response) => {
+  console.log(registration_response);
+})
+.catch(registration_error => {
+  console.log(registration_error);
+})
+
+
+//AXIOS GET
+
+const registration_params = {
+  name: '',
+  surname: '',
+  dateOfBirth: '',
+  password: '',
+};
+
+axios.get('https://api.exaple.com/', {registration_params});
+
+
+//FETCH POST
+
+let fetch_registration_response = await fetch('/article/fetch/post/user', {
+  method: 'POST',
+  headers: {
+    'Content-Type':'aplication/json; carset=utf-8',
+  },
+  body: JSON.stringify(registration_params)
+});
+
+let fetch_registration_result = await fetch_registration_result.json();
+alert(fetch_registration_result.massage);
+
+
+//FETCH GET
+
+fetch('https:://exaple.com/ajax.php')
+.then(response => {
+  return response.json();
+})
+.then(data => {
+  console.dir(data);
+})
+
+//JS POST
+
+const https = require('https');
+const post_js_data = JSON.stringify( {
+  name: '',
+  surname: '',
+  dateOfBirth: '',
+  password: '',
+});
+const post_js_options = {
+  hostname: 'flaviocopes.com',
+  port: 443,
+  path: '/todos',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'aplication/json',
+    'Content-Lenth': data.length,
+  }
+}
+const js_post_require = https.request(post_js_options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+js_post_require.on('error', (error) => {
+  console.error(error)
+})
+js_post_require.write(post_js_data)
+js_post_require.end()
+
+
+//JS GET
+
+const get_js_data = JSON.stringify( {
+  name: '',
+  surname: '',
+  dateOfBirth: '',
+  password: '',
+});
+const get_js_options = {
+  hostname: 'flaviocopes.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET'
+}
+const js_post_require1 = https.request(get_js_options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+js_post_require1.on('error', (error) => {
+  console.error(error)
+})
+js_post_require1.end();

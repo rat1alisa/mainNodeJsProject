fbd42500a7a844c95f31f874861ae82aa5b31849
Alice <alekseevaalisa763@gmail.com>
2024-10-27 14:53:27 +0300

    validation

--- registration/registration.js
+++ registration/registration.js
@@ -114,3 +114,22 @@ js_post_require1.on('error', (error) => {
   console.error(error)
 })
 js_post_require1.end();
+
+//
+
+function validatePassword2() {
+  const passwordField = document.querySelector('.password'); 
+  const password = passwordField.value; 
+  const passwordPattern = /^(?=.*\d)(?=.*[A-Z])[A-Za-z\d]{8,}$/;
+
+  const errorLog = document.getElementById("errorMessage"); 
+
+  if (!passwordPattern.test(password)) {
+      errorLog.innerHTML = "Пароль должен содержать минимум 8 символов, включая 1 заглавную букву и 1 цифру.";
+      return false; 
+  }
+
+  errorLog.innerHTML = ""; 
+  alert('Данные отправлены'); 
+  return true; 
+}
